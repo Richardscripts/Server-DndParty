0e326cfe3be9736361e79c2ce8fa8f20403be54e
unknown <richardcarmenscott@gmail.com>
2020-11-03 11:03:08 -0500

    fixed expiresIn bug

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -7,48 +7,52 @@ const { loginLimiter } = require('../middleware/rate-limiter');
 const authRouter = express.Router();
 const jsonBodyParser = express.json();
 
-authRouter.route('/register').post(jsonBodyParser, (req, res, next) => {
-  let { user_email, password, user_name } = req.body;
-  let newUser = { user_email, password, user_name };
+authRouter
+  .route('/register')
+  .post(loginLimiter, jsonBodyParser, (req, res, next) => {
+    let { user_email, password, user_name } = req.body;
+    let newUser = { user_email, password, user_name };
 
-  if (!user_email || !password || !user_name) {
-    return res.status(400).json({ error: 'Bad Request - Missing Credentials' });
-  }
-  if (password.length < 8 || password.length > 72) {
-    return res.status(400).json({
-      error: 'Password must be atleast 8 characters',
-    });
-  }
-  edc(user_email).then((result) => {
-    if (!result) {
-      return res.status(400).json({ error: 'Invalid Email' });
+    if (!user_email || !password || !user_name) {
+      return res
+        .status(400)
+        .json({ error: 'Bad Request - Missing Credentials' });
     }
-    newUser.password = bcrypt.hashSync(password);
-    AuthService.getUserEmail(req.app.get('db'), user_email).then((result) => {
-      if (result) {
-        return res.status(400).json({ error: 'Email Already Exists' });
+    if (password.length < 8 || password.length > 72) {
+      return res.status(400).json({
+        error: 'Password must be atleast 8 characters',
+      });
+    }
+    edc(user_email).then((result) => {
+      if (!result) {
+        return res.status(400).json({ error: 'Invalid Email' });
       }
-      AuthService.getUsername(req.app.get('db'), user_name).then((result) => {
+      newUser.password = bcrypt.hashSync(password);
+      AuthService.getUserEmail(req.app.get('db'), user_email).then((result) => {
         if (result) {
-          return res.status(400).json({ error: 'Nickname Already Exists' });
+          return res.status(400).json({ error: 'Email Already Exists' });
         }
-        AuthService.registerUser(req.app.get('db'), newUser)
-          .then((user) => {
-            return res.status(201).send({
-              authToken: AuthService.createJwt(user.user_email, {
+        AuthService.getUsername(req.app.get('db'), user_name).then((result) => {
+          if (result) {
+            return res.status(400).json({ error: 'Nickname Already Exists' });
+          }
+          AuthService.registerUser(req.app.get('db'), newUser)
+            .then((user) => {
+              return res.status(201).send({
+                authToken: AuthService.createJwt(user.user_email, {
+                  user_id: user.user_id,
+                  user_name: user.user_name,
+                }),
                 user_id: user.user_id,
-                user_name: user.user_name,
-              }),
-              user_id: user.user_id,
-            });
-          })
-          .catch(next);
+              });
+            })
+            .catch(next);
+        });
       });
     });
   });
-});
 
-authRouter.post('/login', jsonBodyParser, (req, res, next) => {
+authRouter.post('/login', loginLimiter, jsonBodyParser, (req, res, next) => {
   const { user_email, password } = req.body;
   const loginUser = { user_email, password };
 
--- src/server.js
+++ src/server.js
@@ -11,7 +11,6 @@ const db = knex({
 });
 
 app.set('db', db);
-// app.set('trust proxy', 1);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
